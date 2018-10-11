2f7c43bc37736afb42ccc3293ff6e794eb5dada2
bohrytsenko <hrytsenkobohdan@gmail.com>
2018-10-11 14:48:46 +0200

    style changes

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -6,9 +6,31 @@ $( document ).ready(function() {
         .dropdown();
 
     // Rating
+
     $('.ui.rating')
         .rating('disable')
     ;
+
+    // Bar
+
+    $(".bar").each(function(){
+        $(this).find(".bar-inner").animate({
+            width: $(this).attr("data-width")
+        },2000)
+    });
+
+    // Navigation
+
+
+    $('.header_wrap_row__nav a').on('click', function () {
+        $('.header__over').toggleClass('header_over_active');
+        $('.header__navigation').toggleClass('header__fixed');
+    });
+
+    $('.header__over').on('click', function () {
+        $(this).removeClass('header_over_active');
+        $('.header__navigation').removeClass('header__fixed');
+    });
 });
 
 
